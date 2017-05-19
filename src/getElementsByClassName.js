50cd8239c3f84e429210ee370ecfca3c5205f02c
maximilian183 <max.kwok@gmail.com>
2017-05-19 11:52:35 -0700

    getElementsByClassName.js done!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,29 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // initialize array of class names
+  // create function
+      // check class list for classname
+      // if classname is found, push to array
+      // loop through elements in childnodes
+      // recursive call on each childnode
+
+  if (elementArr === undefined) {
+    var elementArr = [];
+  }
+
+  var searchElements = function(element) {
+    if (element.classList === undefined) {
+      // if undefined, do nothing
+    } else if (element.classList.contains(className)) {
+      elementArr.push(element);
+    }
+
+    for ( var i = 0; i < element.childNodes.length; i++) {
+      searchElements(element.childNodes[i]);
+    }
+  };
+
+  searchElements(document.body);
+  return elementArr;
 };
